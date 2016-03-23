893d937bb07dd7bba092fbcb2e1eb5c4d07b882f
Stuart Leyland <stuartleyland@gmail.com>
2016-03-23 21:46:22 +0000

    Added controller for shipwrecks (following tutorial) to perform CRUD operations

--- src/main/java/com/packedit/controller/HomeController.java
+++ src/main/java/com/packedit/controller/HomeController.java
@@ -1,10 +1,10 @@
-package com.packedit;
+package com.packedit.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloController {
+public class HomeController {
 
     @RequestMapping("/")
     public String index() {
---
+++ src/main/java/com/packedit/model/Shipwreck.java
@@ -0,0 +1,91 @@
+package com.packedit.model;
+
+public class Shipwreck {
+    Long id;
+    String name;
+    String description;
+    String condition;
+    Integer depth;
+    Double latitude;
+    Double longitude;
+    Integer yearDiscovered;
+
+    public Shipwreck() {
+    }
+
+    public Shipwreck(final Long id, final String name, final String description, final String condition, final Integer depth, final Double latitude,
+            final Double longitude, final Integer yearDiscovered) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.condition = condition;
+        this.depth = depth;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.yearDiscovered = yearDiscovered;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(final Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(final String description) {
+        this.description = description;
+    }
+
+    public String getCondition() {
+        return condition;
+    }
+
+    public void setCondition(final String condition) {
+        this.condition = condition;
+    }
+
+    public Integer getDepth() {
+        return depth;
+    }
+
+    public void setDepth(final Integer depth) {
+        this.depth = depth;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(final Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(final Double longitude) {
+        this.longitude = longitude;
+    }
+
+    public Integer getYearDiscovered() {
+        return yearDiscovered;
+    }
+
+    public void setYearDiscovered(final Integer yearDiscovered) {
+        this.yearDiscovered = yearDiscovered;
+    }
+}
