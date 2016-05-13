3f3deba2719a26056f84e9bc3e951daaac7b21b3
Stuart Leyland <stuartleyland@gmail.com>
2016-05-13 19:16:27 +0100

    Included Bootstrap styling and fixed a deprecated React Router API

--- frontend/server.jsx
+++ frontend/server.jsx
@@ -1,7 +1,7 @@
 import express                   from 'express';
 import React                     from 'react';
 import { renderToString }        from 'react-dom/server'
-import { RoutingContext, match } from 'react-router';
+import { RouterContext, match } from 'react-router';
 import createLocation            from 'history/lib/createLocation';
 import routes                    from 'routes';
 
@@ -16,15 +16,20 @@ app.use((req, res) => {
     if (!renderProps) return res.status(404).end('Not found.');
 
     const InitialComponent = (
-      <RoutingContext {...renderProps} />
+      <RouterContext {...renderProps} />
     );
     const componentHTML = renderToString(InitialComponent);
     const HTML = `
     <!DOCTYPE html>
     <html>
       <head>
-        <meta charset="utf-8">
-        <title>Isomorphic Redux Demo</title>
+        <meta charset="UTF-8">
+        <title>Packed It</title>
+        <link rel="stylesheet" href="/style.css" />
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous" />
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous" />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
       </head>
       <body>
         <div id="react-view">${componentHTML}</div>
