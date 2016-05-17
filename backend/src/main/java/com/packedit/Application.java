6ebce47ee2465e0636cbcd7efa30b5263088a41b
Stuart Leyland <stuartleyland@gmail.com>
2016-05-17 16:34:52 +0100

    Add Auth0 for authentication

--- backend/src/main/java/com/packedit/Application.java
+++ backend/src/main/java/com/packedit/Application.java
@@ -2,12 +2,20 @@ package com.packedit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableJpaRepositories
 @EnableTransactionManagement
+@Configuration
+@ComponentScan("com.auth0")
+@ImportResource("classpath:auth0-security-context.xml")
+@PropertySource("classpath:auth0.properties")
 public class Application {
 
     public static void main(final String[] args) {
--- frontend/shared/components/App.js
+++ frontend/shared/components/App.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import ListsOverview from './ListsOverview.js';
+import {AUTH0_CLIENT_ID, AUTH0_DOMAIN} from '../../auth0-variables.js';
 
 export default class App extends React.Component {
+
+  constructor() {
+    super();
+    this.lock = new Auth0Lock(AUTH0_CLIENT_ID, AUTH0_DOMAIN);
+    this.showLock = this.showLock.bind(this);
+  }
+
+  showLock() {
+    this.lock.show();
+  }
+
   render() {
     return (
-    	<ListsOverview />
+      <div>
+        <div className="login-box">
+        <a onClick={this.showLock}>Sign In</a>
+        </div>
+      	<ListsOverview />
+      </div>
     );
   }
 }
