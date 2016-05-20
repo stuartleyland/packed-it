c568a5f9a2d0bf22185f754acf827b414bb5805d
Stuart Leyland <stuartleyland@gmail.com>
2016-05-20 15:38:00 +0100

    Removed Shipwreck entity and related files from initial demo

--- backend/src/main/java/com/packedit/repository/ShipwreckRepository.java
+++
@@ -1,9 +0,0 @@
-package com.packedit.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.packedit.model.Shipwreck;
-
-public interface ShipwreckRepository extends JpaRepository<Shipwreck, Long> {
-
-}
